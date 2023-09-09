42dc2e426d49a37d943943b7b0b51c813bf8dd87
iamseun <seunayela@gmail.com>
2023-09-09 12:53:48 +0100

    changed the github file url

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import datetime
 
 app = Flask(__name__)
 
+
 @app.route('/get_stage1', methods=['GET'])
 def get_stage1():
     slack_name = request.args.get('slack_name')
@@ -10,15 +11,15 @@ def get_stage1():
 
     current_day = datetime.datetime.utcnow().strftime("%A")
     utc_time = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
-  
 
-    current_utc_time = datetime.datetime.strptime(utc_time, "%Y-%m-%dT%H:%M:%SZ")
+    current_utc_time = datetime.datetime.strptime(
+        utc_time, "%Y-%m-%dT%H:%M:%SZ")
     two_hours_ago = datetime.datetime.utcnow() - datetime.timedelta(hours=2)
     two_hours_later = datetime.datetime.utcnow() + datetime.timedelta(hours=2)
-    
+
     status_code = 200 if two_hours_ago <= current_utc_time <= two_hours_later else 400
-    
-    github_file_url = "https://github.com/username/repo/blob/main/app.py"
+
+    github_file_url = "https://github.com/Seun-Ayela/hngx/blob/main/app.py"
     github_repo_url = "https://github.com/Seun-Ayela/hngx.git"
 
     response = {
@@ -33,6 +34,6 @@ def get_stage1():
 
     return jsonify(response)
 
+
 if __name__ == '__main__':
     app.run(debug=True, port=8090)
-
